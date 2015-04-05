cc76a1434602ef309f4e9dfc76b1a8c79bf34a1e
ts25504 <ts25504@163.com>
2015-04-05 22:35:53 +0800

    Add rich-text support.

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,42 +1,43 @@
 from flask.ext.wtf import Form
+from flask.ext.pagedown.fields import PageDownField
 from wtforms import StringField, TextAreaField, SubmitField, IntegerField, \
         SelectField
 from wtforms.validators import Required, Length
 from ..models import SingleChoice, BlankFill, Essay
 
 class SingleChoiceForm(Form):
-    question = TextAreaField('New Question', validators=[Required()])
-    A = StringField('A', validators=[Required(), Length(1, 64)])
-    B = StringField('B', validators=[Required(), Length(1, 64)])
-    C = StringField('C', validators=[Required(), Length(1, 64)])
-    D = StringField('D', validators=[Required(), Length(1, 64)])
+    question = PageDownField('New Question', validators=[Required()])
+    A = StringField('A', validators=[Required(), Length(1, 255)])
+    B = StringField('B', validators=[Required(), Length(1, 255)])
+    C = StringField('C', validators=[Required(), Length(1, 255)])
+    D = StringField('D', validators=[Required(), Length(1, 255)])
     score = IntegerField('score', validators=[Required()])
     difficult_level = SelectField('difficult_level',
             choices=[('A', 'A'), ('B', 'B'), ('C', 'C'), ('D', 'D'),
                 ('E', 'E')])
-    faq = TextAreaField('faq')
+    faq = PageDownField('faq')
     answer = SelectField('answer', choices=[('A', 'A'), ('B', 'B'),
         ('C', 'C'), ('D', 'D')], validators=[Required()])
     submit = SubmitField('Submit')
 
 class BlankFillForm(Form):
-    question = TextAreaField('New Question', validators=[Required()])
+    question = PageDownField('New Question', validators=[Required()])
     difficult_level = SelectField('difficult_level',
             choices=[('A', 'A'), ('B', 'B'), ('C', 'C'), ('D', 'D'),
                 ('E', 'E')])
-    faq = TextAreaField('faq')
+    faq = PageDownField('faq')
     score = IntegerField('score', validators=[Required()])
-    answer = StringField('answer', validators=[Required(), Length(1, 64)])
+    answer = StringField('answer', validators=[Required(), Length(1, 255)])
     submit = SubmitField('Submit')
 
 class EssayForm(Form):
-    question = TextAreaField('New Question', validators=[Required()])
+    question = PageDownField('New Question', validators=[Required()])
     difficult_level = SelectField('difficult_level',
             choices=[('A', 'A'), ('B', 'B'), ('C', 'C'), ('D', 'D'),
                 ('E', 'E')])
-    faq = TextAreaField('faq')
+    faq = PageDownField('faq')
     score = IntegerField('score', validators=[Required()])
-    answer = TextAreaField('answer', validators=[Required()])
+    answer = PageDownField('answer', validators=[Required()])
     submit = SubmitField('Submit')
 
 class DeleteForm(Form):
