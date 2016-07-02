39b0bfc5d354b92a04f4917b12293ed1ad2b4cc6
ts25504 <ts25504@163.com>
2016-07-02 10:24:00 +0800

    fix package bugs

--- app/api_1_0/users.py
+++ app/api_1_0/users.py
@@ -3,6 +3,12 @@ from app.api_1_0 import api
 from app.model.user_model import User
 
 
+@api.route('/users/')
+def get_users():
+    users = User.query.all()
+    return jsonify(
+        {'users': [users.to_json()] for u in users})
+
 @api.route('/users/<int:id>')
 def get_user(id):
     user = User.query.get_or_404(id)
